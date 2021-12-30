7b3f0560511439e1851dc45558bf5334f80142ef
putriindah123 <95621953+putriindah123@users.noreply.github.com>
2021-12-30 20:54:30 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const model = require('./sdk/model.js'); //predict
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5057562264:AAHLUz5s6G-8vDMtapgD6l1Y4Z2i5su7OlM'
+const token = '5039010009:AAGIH7_hkZ2yHE6XdjCXs2IGOG47JhngwSE'
 const bot = new TelegramBot(token, {polling: true});
 
 state=0;
